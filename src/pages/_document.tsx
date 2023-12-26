757371ee4020574f56d0e925dbc1bfcd86ddeffb
Gurpreet <gs109690@gmail.com>
2023-12-25 19:02:50 -0600

    feat: header title tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <title>Guide - Next.js | Connectkit | Wagmi</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
